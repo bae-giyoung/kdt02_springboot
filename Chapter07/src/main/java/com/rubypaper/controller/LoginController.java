6b69a498191c83aab2a3f561c9f51bb1045a5295
bae-giyoung <giyoung1012@gmail.com>
2025-08-19 14:12:21 +0900

    Spring Security 암호화 실습, 회원가입 페이지

--- Chapter07/src/main/java/com/rubypaper/controller/LoginController.java
+++ Chapter07/src/main/java/com/rubypaper/controller/LoginController.java
@@ -1,16 +1,23 @@
 package com.rubypaper.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rubypaper.config.SecurityUser;
+import com.rubypaper.domain.Member;
+import com.rubypaper.service.MemberService;
 
 @Controller
 public class LoginController {
+	@Autowired
+	MemberService memberService;
+	
 	@GetMapping("/login")
 	public void login() {
 		System.out.println("login 요청");
@@ -41,5 +48,15 @@ public class LoginController {
 		System.out.println(auth.getPrincipal());
 		System.out.println(auth.getCredentials());
 		System.out.println(auth.getAuthorities());
+	}
+	
+	// 회원 가입 기능 추가
+	@GetMapping("/join")
+	public void join() {}
+	
+	@PostMapping("/join")
+	public String joinProc(Member member) {
+		memberService.save(member);
+		return "welcome";
 	}
 }
