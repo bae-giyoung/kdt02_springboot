9661e2f3d7d58d0eb4b65e24b2e8584ddb05b726
bae-giyoung <giyoung1012@gmail.com>
2025-08-20 18:38:31 +0900

    Spring Security 복습

---
+++ Chapter07-3/src/main/java/edu/pnu/Chapter073Application.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter073Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter073Application.class, args);
+	}
+
+}
